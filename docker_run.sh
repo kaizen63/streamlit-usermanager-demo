07ec79d3a99db43a54ce6361880d17b78278ca7b
Kai Poitschke <kai@poitschke.de>
2024-12-08 16:26:16 +0100

    only start safari when container starts

--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker compose -f docker-compose.yml up --build -d
-sleep 1
-open http://localhost:18501?debug=1
+docker compose -f docker-compose.yml --progress=plain up --build -d \
+&& sleep 1 \
+&& open http://localhost:18501?debug=1
 
 
